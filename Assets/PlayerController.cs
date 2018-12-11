b32f0db4c3c23ba07df8bfd7cc0fe2938ad9fe19
bill-bateman <williamsbateman@gmail.com>
2018-12-11 13:19:10 -0500

    Initial commit.

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	//movement variables
+	private float movement;
+	private bool btn_jump, btn_left, btn_right;
+
+	//keyboard bindings
+	private KeyCode keyboard_jump_button = KeyCode.Space;
+	private KeyCode keyboard_movement_left = KeyCode.LeftArrow;
+	private KeyCode keyboard_movement_right = KeyCode.RightArrow;
+
+
+	// Getter functions
+
+	public bool get_jump() {
+		if (btn_jump) {
+			btn_jump = false;
+			return true;
+		} 
+		return btn_jump;
+	}
+
+	public bool is_moving() {
+		return btn_left ^ btn_right;
+	}
+
+	public float get_movement() { return movement; }
+
+
+
+	// Use this for initialization
+	void Start () {
+		btn_jump = btn_left = btn_right = false;
+		movement = 0.0f;
+	}
+		
+	// Update is called once per frame
+	void Update () {
+		btn_left = Input.GetKey (keyboard_movement_left);
+		btn_right = Input.GetKey (keyboard_movement_right);
+
+		//left-right movement
+		if (btn_left ^ btn_right) {
+			movement = (btn_left ? -1.0f : 1.0f);
+		} else {
+			movement = 0.0f;
+		}
+
+		//jump
+		if (!btn_jump) btn_jump = (Input.GetKeyDown(keyboard_jump_button));
+	}
+}
---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public InputModule input_module;
+	public float speed;
+	public float jump_force = 500.0f;
+	public Animator player_animator;
+	public SpriteRenderer player_renderer;
+
+	private Rigidbody2D rb;
+	private bool is_on_ground;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponentInChildren<Rigidbody2D> ();
+		is_on_ground = true;
+	}
+	
+	// FixedUpdate is called at constant intervals
+	void FixedUpdate () {
+		/*** Player motion ***/
+		if (input_module.is_moving ()) {
+			//horizontal movement
+			transform.localPosition += new Vector3 (input_module.get_movement () * (speed * Time.deltaTime), 0); //control movement speed
+			player_renderer.flipX = input_module.get_movement()<0 ? true : false;
+			player_animator.SetBool ("is_walking", true);
+		} else {
+			player_animator.SetBool("is_walking", false);
+		}
+
+
+		if (input_module.get_jump () && is_on_ground) {
+			Vector3 up = new Vector3 (0, 1);
+			rb.AddForce (jump_force * up);
+		}
+	}
+}
