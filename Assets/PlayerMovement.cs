17254aad0a5453f0c83017bcd3d6eea71ebb9303
bill-bateman <williamsbateman@gmail.com>
2018-12-13 18:02:08 -0500

    Added jumping animation.

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -9,15 +9,41 @@ public class PlayerMovement : MonoBehaviour {
 	public float jump_force = 500.0f;
 	public Animator player_animator;
 	public SpriteRenderer player_renderer;
+	public float min_dist_to_ground = 0.5f;
 
 	private Rigidbody2D rb;
-	private bool is_on_ground;
+	private bool is_on_ground, old_ground;
+	private bool jumped;
 
 	// Use this for initialization
 	void Start () {
-		rb = GetComponentInChildren<Rigidbody2D> ();
-		is_on_ground = true;
+		rb = GetComponent<Rigidbody2D> ();
+		is_on_ground = jumped = false;
+		old_ground = true; //fall
 	}
+
+	//called on entering collision (where one of the 2 objects must have isTrigger checked)
+	void OnTriggerEnter2D(Collider2D other) {
+		if (other.tag == "ground") {
+			old_ground = is_on_ground;
+			is_on_ground = true;
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D other) {
+		if (other.tag == "ground") {
+			old_ground = is_on_ground;
+			is_on_ground = false;
+		}
+	}
+
+	void OnTriggerStay2D(Collider2D other) {
+		if (other.tag == "ground") {
+			old_ground = is_on_ground;
+			is_on_ground = true;
+		}
+	}
+
 	
 	// FixedUpdate is called at constant intervals
 	void FixedUpdate () {
@@ -31,10 +57,44 @@ public class PlayerMovement : MonoBehaviour {
 			player_animator.SetBool("is_walking", false);
 		}
 
+		if (is_on_ground) {
+			if (!old_ground) {
+				//just landed
+				jumped = false;
+				player_animator.SetBool ("is_jumping", false);
+				player_animator.SetBool ("is_falling", false);
+			}
 
-		if (input_module.get_jump () && is_on_ground) {
-			Vector3 up = new Vector3 (0, 1);
-			rb.AddForce (jump_force * up);
+			//perform jump
+			if (!jumped && input_module.get_jump ()) {
+				jumped = true;
+				is_on_ground = false; old_ground = true;
+				Vector3 up = new Vector3 (0, 1);
+				rb.AddForce (jump_force * up);
+			}
+
+		} 
+		if (!is_on_ground) {
+			if (old_ground) {
+				//just jumped or fell
+				if (jumped) {
+					//just jumped
+					player_animator.SetBool ("is_jumping", true);
+				} else {
+					//just fell 
+					player_animator.SetBool ("is_falling", true);
+				}
+			}
+
+			if (jumped && rb.velocity.y < 0) {
+				//change from jumping to falling
+				jumped = false;
+				player_animator.SetBool ("is_falling", true);
+			}
+
 		}
+
+
+		old_ground = is_on_ground;
 	}
 }
