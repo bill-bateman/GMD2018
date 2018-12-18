6ae65a27bfbe892e8351719cf9e1bbaedc6fbe5a
bill-bateman <williamsbateman@gmail.com>
2018-12-18 16:42:30 -0500

    Adding backgrounds and parallax.

---
+++ Assets/ParallaxScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParallaxScript : MonoBehaviour
+{
+	public GameObject to_track;
+	public float speed;
+
+	private float prev_x;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		prev_x = to_track.transform.position.x;
+    }
+
+    // FixedUpdate is called at a constant rate
+    void FixedUpdate()
+    {
+		float x = to_track.transform.position.x;
+
+		transform.position += new Vector3((x - prev_x) * speed, 0);
+
+		prev_x = x;
+    }
+}
